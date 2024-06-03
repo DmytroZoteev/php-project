2fd1b0b2110cba96d2e906e3c0d930fc06eedb02
Dmytro Zoteev <137426491+DmytroZoteev@users.noreply.github.com>
2024-06-03 13:21:57 +0000

    create

--- blog/views/post/index.php
+++ blog/views/post/index.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <title>Blog</title>
+    <link rel="stylesheet" href="/public/css/style.css">
 </head>
 <body>
     <h1>Blog Posts</h1>
@@ -12,9 +13,11 @@
             <li>
                 <a href="/post/show?id=<?= $post['id'] ?>"><?= $post['title'] ?></a>
                 <a href="/post/edit?id=<?= $post['id'] ?>">Edit</a>
-                <a href="/post/delete?id=<?= $post['id'] ?>">Delete</a>
+                <a href="/post/delete?id=<?= $post['id'] ?>" class="delete">Delete</a>
             </li>
         <?php endforeach; ?>
     </ul>
+    <script src="/public/js/script.js"></script>
 </body>
 </html>
+
