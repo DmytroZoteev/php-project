85565cee756ab7b2567e7ca9c521ad5d234eaad8
Dmytro Zoteev <137426491+DmytroZoteev@users.noreply.github.com>
2024-06-03 13:30:01 +0000

    fix

--- blog/controllers/PostController.php
+++ blog/controllers/PostController.php
@@ -1,11 +1,15 @@
 <?php
 
-require_once 'models/Post.php';
 require_once 'core/View.php';
 
 class PostController {
+    private $posts = [
+        ['id' => 1, 'title' => 'First Post', 'content' => 'This is the content of the first post.', 'created_at' => '2023-01-01 00:00:00'],
+        ['id' => 2, 'title' => 'Second Post', 'content' => 'This is the content of the second post.', 'created_at' => '2023-01-02 00:00:00'],
+    ];
+
     public function index() {
-        $posts = Post::all();
+        $posts = $this->posts;
         View::render('post/index', ['posts' => $posts]);
     }
 
@@ -14,36 +18,39 @@ class PostController {
     }
 
     public function store() {
-        $title = $_POST['title'];
-        $content = $_POST['content'];
-        Post::create($title, $content);
+        // Симуляція збереження даних
         header('Location: /');
     }
 
     public function edit() {
         $id = $_GET['id'];
-        $post = Post::find($id);
+        $post = $this->findPostById($id);
         View::render('post/edit', ['post' => $post]);
     }
 
     public function update() {
-        $id = $_POST['id'];
-        $title = $_POST['title'];
-        $content = $_POST['content'];
-        Post::update($id, $title, $content);
+        // Симуляція оновлення даних
         header('Location: /');
     }
 
     public function delete() {
-        $id = $_GET['id'];
-        Post::delete($id);
+        // Симуляція видалення даних
         header('Location: /');
     }
 
     public function show() {
         $id = $_GET['id'];
-        $post = Post::find($id);
+        $post = $this->findPostById($id);
         View::render('post/show', ['post' => $post]);
+    }
+
+    private function findPostById($id) {
+        foreach ($this->posts as $post) {
+            if ($post['id'] == $id) {
+                return $post;
+            }
+        }
+        return null;
     }
 }
 ?>
